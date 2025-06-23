a169e5d2ffbce56557f947aabcaa70887709c298
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2025-06-23 20:39:23 +0900

    add freezed package

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -11,18 +11,15 @@ abstract class HotPepperEndpoints {
     //検索用ディナー予算マスタAPI
     budget: '$_baseUrl/budget/v1/$_baseParams',
     //大サービスエリアマスタAPI
-    area: (
-      largeServiceArea:
-          '$_baseUrl/large_service_area/v1/$_baseParams',
-      //サービスエリアマスタAPI
-      serviceArea: '$_baseUrl/service_area/v1/$_baseParams',
-      //大エリアマスタAPI
-      largeArea: '$_baseUrl/large_area/v1/$_baseParams',
-      //中エリアマスタAPI
-      middleArea: '$_baseUrl/middle_area/v1/$_baseParams',
-      //小エリアマスタAPI
-      smallArea: '$_baseUrl/small_area/v1/$_baseParams',
-    ),
+    largeServiceArea: '$_baseUrl/large_service_area/v1/$_baseParams',
+    //サービスエリアマスタAPI
+    serviceArea: '$_baseUrl/service_area/v1/$_baseParams',
+    //大エリアマスタAPI
+    largeArea: '$_baseUrl/large_area/v1/$_baseParams',
+    //中エリアマスタAPI
+    middleArea: '$_baseUrl/middle_area/v1/$_baseParams',
+    //小エリアマスタAPI
+    smallArea: '$_baseUrl/small_area/v1/$_baseParams',
     //ジャンルマスタAPI
     genre: '$_baseUrl/genre/v1/$_baseParams',
     //クレジットカードマスタAPI
