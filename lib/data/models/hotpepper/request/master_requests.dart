a7232d152cb4f9011c18254ebc254635463d537b
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2025-06-25 23:43:15 +0900

    delete master_request model

--- lib/data/models/hotpepper/request/master_requests.dart
+++ lib/data/models/hotpepper/request/master_requests.dart
@@ -1,22 +1,17 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
+// 현재 Budget Master API는 추가 파라미터가 필요 없어서
+// Request 모델이 불필요합니다.
+// 
+// format=json과 API key는 이미 HotPepperEndpoints에서
+// 자동으로 추가되므로 별도 Request 클래스가 필요하지 않습니다.
+//
+// 향후 다른 Master API에서 파라미터가 필요한 경우
+// 해당 API용 Request 모델을 이 파일에 추가할 수 있습니다.
 
-part 'master_requests.freezed.dart';
-part 'master_requests.g.dart';
-
-@freezed
-class BudgetMasterRequest with _$BudgetMasterRequest {
-  const factory BudgetMasterRequest() = _BudgetMasterRequest;
-
-  factory BudgetMasterRequest.fromJson(Map<String, dynamic> json) =>
-      _$BudgetMasterRequestFromJson(json);
-}
-
-extension BudgetMasterRequestX on BudgetMasterRequest {
-  Map<String, String> toQueryParams() {
-    return const <String, String>{};
-  }
-
-  bool isValid() => true;
-
-  static const BudgetMasterRequest instance = BudgetMasterRequest();
-}
+// 예시:
+// @freezed
+// class GenreMasterRequest with _$GenreMasterRequest {
+//   const factory GenreMasterRequest({
+//     String? keyword,
+//     int? count,
+//   }) = _GenreMasterRequest;
+// }
